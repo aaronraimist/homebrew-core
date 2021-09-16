3d148a0f4eb54f513a81646aad96958a24ac1b7f
Aaron Raimist <aaron@raim.ist>
2021-09-15 19:56:06 -0500

    libolm 3.2.5

--- Formula/libolm.rb
+++ Formula/libolm.rb
@@ -1,8 +1,8 @@
 class Libolm < Formula
   desc "Implementation of the Double Ratchet cryptographic ratchet"
   homepage "https://gitlab.matrix.org/matrix-org/olm"
-  url "https://gitlab.matrix.org/matrix-org/olm/-/archive/3.2.4/olm-3.2.4.tar.gz"
-  sha256 "d6033363fe27bb1ad88940f2acfff5517b1ba9e32b556f5b0fa4c365e5ec892a"
+  url "https://gitlab.matrix.org/matrix-org/olm/-/archive/3.2.5/olm-3.2.5.tar.gz"
+  sha256 "9c7938ce1bb6c46c6712e51341d9e70ed9f875e091bd5c80558f1afddc2d0a11"
   license "Apache-2.0"
 
   bottle do
